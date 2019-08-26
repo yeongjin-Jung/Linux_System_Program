695873e7af4813525e450a59d75af92a83421aab
yeongjin133 <53737175+yeongjin133@users.noreply.github.com>
2019-08-26 10:10:32 +0900

    Create pthread.c

---
+++ pthread.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <pthread.h>    // 스레드 사용하기 위한 헤더파일
+
+//전역 변수 선언
+int glob_var = 10;
+
+void* t_function(void* data)    //void* 주소값만 넘기겠다 , 
+                                //void* data 주소값을 받겠다
+{   
+    int argData;
+    pthread_t t_id;
+    argData = *((int*)data);
+    t_id = pthread_self();
+    glob_var +=3;
+    printf("getpid =%d,t_id=%lu, argData = %d, glob_var = %d\n"
+            ,getpid(), t_id, argData, glob_var);
+    sleep(argData);
+    return (void*)(argData*argData);
+
+}
+
+int main(void)
+{   
+    // POSIX thread id data type
+    pthread_t p_thread[2];
+    int ret;
+    int a;
+    int b;
+    int status;
+
+    //프로세스 아이디값
+    printf("PID = %d\n", getpid());
+   
+    //Thread 1
+    ret = pthread_create(&p_thread[0],NULL,t_function, (void*)&a);
+    if(ret <0) //pthread_create 정상적으로 수행되지 않은 경우
+    {
+        perror("Error: pthread_create() : thread1");
+        return -1;
+    }
+    //Thread 2
+    ret = pthread_create(&p_thread[1],NULL,t_function, (void*)&b);
+    if(ret <0) //pthread_create 정상적으로 수행되지 않은 경우
+    {   
+          perror("Error: pthread_create() : thread1");
+          return -1;
+    }
+    pthread_join(p_thread[0],(void**)&status);
+    printf("thread_join1:%d\n",status);
+
+    pthread_join(p_thread[1],(void**)&status);
+    printf("thread_join2:%d\n",status);
+
+
+}
+
